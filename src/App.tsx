eeea54a8dac5fbdf4dd66a134a099848d13968a1
Abhishek N <abhishekpes123@gmail.com>
2026-06-05 14:03:30 +0530

    updated all pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 
+import { Routes, Route } from "react-router-dom";
+
 import AboutPage from "./pages/About/AboutPage";
+import FaqPage from "./pages/Faq/FaqPage";
+import TeamPage from "./pages/Team/TeamPage";
+import HomePage from "./pages/Home/HomePage";
+import ServicesPage from "./pages/Services/ServicesPage";
+import ProjectPage from "./pages/Projects/ProjectPage";
+import NewsPage from "./pages/News/NewsPage";
+import ContactPage from "./pages/Contact/ContactPage";
+
 
 export default function App() {
   return (
-    <Layout>
-      <AboutPage />
-    </Layout>
+ 
+      <Layout>
+        <Routes>
+
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Layout>
+
   );
 }
