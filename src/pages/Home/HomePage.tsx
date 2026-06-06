adcfb21e72fd3a0553f28a5a1082284c0b6b21da
Abhishek N <abhishekpes123@gmail.com>
2026-06-06 15:34:28 +0530

    home services

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import HomeAbout from "../../components/Home/HomeAbout";
 import HomeGetTouch from "../../components/Home/HomeGetTouch";
 import HomeHero from "../../components/Home/HomeHero";
 import Homeourwork from "../../components/Home/Homeourwork";
+import HomeServices from "../../components/Home/HomeServices";
 import HomeWhychooseus from "../../components/Home/HomeWhychooseus";
 import HomeWorkprocess from "../../components/Home/HomeWorkprocess";
 
@@ -11,6 +12,7 @@ export default function HomePage() {
     <div>
       <HomeHero/>
       <HomeAbout/>
+      <HomeServices/>
       <HomeWhychooseus/>
       <Homeourwork/>
       <HomeWorkprocess/>
